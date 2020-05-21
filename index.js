155cc9882e83ec72a97c105fe0c9a741d04c4974
Cflip <36554078+cflip@users.noreply.github.com>
2020-05-21 14:06:20 -0600

    Basic Markov chain implementation

--- index.js
+++ index.js
@@ -3,15 +3,43 @@ const client = new Discord.Client();
 
 const {token} = require("./token.json");
 
-// Send a message to all servers when bot comes online
-client.on("ready", () => {
-	client.guilds.cache.forEach(guild => {
-		// Get the 'general' channel if one exists
-		const general = guild.channels.cache.find(ch => ch.name === "general");
-		if (!general) return;
+var ngrams = {};
+var beginnings = [];
+
+function addToChain(string) {
+	var words = string.split(" ");
 	
-		general.send("MirrorBot is now online!");
-	});
+	for (var i = 0; i < words.length; i++) {
+		var gram = words[i];
+		
+		if (i==0) beginnings.push(gram);
+		if (!ngrams[gram]) ngrams[gram] = [];
+
+		ngrams[gram].push(words[i+1]);
+	}
+}
+
+function getMessage(length) {
+	var currentGram = beginnings[Math.floor(Math.random()*beginnings.length)];
+	var result = currentGram;
+
+	for (var i = 0; i < length; i++) {
+		var possibilites = ngrams[currentGram];
+		if (possibilites == undefined) continue;
+		var next = possibilites[Math.floor(Math.random()*possibilites.length)];
+		if (next == undefined) continue;
+		result += " " + next;
+		currentGram = next;
+	}
+
+	return result;
+}
+
+client.on("message", message => {
+	if (message.member.user !== client.user) {
+		addToChain(message.content);
+		if (message.mentions.has(client.user)) message.channel.send(getMessage(100));
+	}
 });
 
 client.login(token);
