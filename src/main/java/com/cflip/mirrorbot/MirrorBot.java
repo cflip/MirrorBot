2015e5ed739cc5d696a50e8ef254845a35b8dcc5
Cflip <36554078+cflip@users.noreply.github.com>
2020-07-06 22:30:45 -0600

    Respond to messages when mentioned

--- src/main/java/com/cflip/mirrorbot/MirrorBot.java
+++ src/main/java/com/cflip/mirrorbot/MirrorBot.java
@@ -3,6 +3,7 @@ package com.cflip.mirrorbot;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.User;
 import discord4j.core.object.entity.channel.MessageChannel;
 
 public class MirrorBot {
@@ -11,15 +12,16 @@ public class MirrorBot {
 		if (token == null) token = args[0];
 
 		GatewayDiscordClient client = DiscordClientBuilder.create(token).build().login().block();
+	 	User self = client.getSelf().block();
 		Chain chain = new Chain();
 
 		client.getEventDispatcher().on(MessageCreateEvent.class)
 			.map(MessageCreateEvent::getMessage)
-			.filter(message -> !message.getAuthor().map(user -> user.equals(client.getSelf().block())).orElse(false))
+			.filter(message -> !message.getAuthor().map(user -> user.equals(self)).orElse(false))
 			.subscribe(message -> {
 				chain.add(message.getContent().split(" "));
 
-				if (Math.random() < 0.15) {
+				if (Math.random() < 0.15 || message.getUserMentionIds().contains(self.getId())) {
 					MessageChannel channel = message.getChannel().block();
 					String msg = chain.createMessage(150);
 					if (msg != null) channel.createMessage(msg).block();
